2dec0399d38ad1bdfa4a849b6fe2e160e61efab6
Nallamilli Reddy S G B Rama Krishna Reddy <nallamilli.reddy@bounteous.com>
2026-03-10 11:24:06 +0530

    Implemented User Booking.

---
+++ src/UserBooking.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class UserBooking implements Runnable {
+    private final TicketBooking ticketBooking;
+    private final String userName;
+    private final Random random = new Random();
+
+    public UserBooking(TicketBooking ticketBooking, String userName) {
+        this.ticketBooking = ticketBooking;
+        this.userName = userName;
+    }
+
+    @Override
+    public void run() {
+
+        int reqTickets = random.nextInt(3) + 1;
+
+        try {
+            ticketBooking.bookTickets(userName,reqTickets);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
