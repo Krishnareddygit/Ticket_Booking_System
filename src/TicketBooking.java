0de6e26765e5dbf0b4bd64ba7b8f8681a38a11ec
Nallamilli Reddy S G B Rama Krishna Reddy <nallamilli.reddy@bounteous.com>
2026-03-10 11:13:49 +0530

    Implemented Ticket Booking Logic

---
+++ src/TicketBooking.java
@@ -0,0 +1,29 @@
+public class TicketBooking {
+    private int availableTickets;
+    private int totalSoldTickets = 0;
+
+    public TicketBooking(int availableTickets){
+        this.availableTickets = availableTickets;
+    }
+
+    public synchronized void bookTickets(String user, int reqTickets) throws InterruptedException {
+        while (reqTickets > availableTickets) {
+
+            System.out.println(user + " requested " + reqTickets + " tickets → Waiting (Not enough tickets)");
+
+            try {
+                wait();
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+
+        availableTickets -= reqTickets;
+        totalSoldTickets += reqTickets;
+        System.out.println(user + " requested " + reqTickets + " tickets → Booked → Remaining: " + availableTickets);
+
+        notifyAll();
+    }
+
+}
